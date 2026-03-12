f852988e7de4c8f3cc3ff0eb7582d35029347570
cesareye <cesar.arvelo@escola.pr.gov.br>
2026-03-12 12:31:24 -0300

    Add files via upload

---
+++ python.py
@@ -0,0 +1,11 @@
+cardapio =["cafe", "capuccino","agua","pao frances" ]
+preco = [6.00,12.00, 3.00, 15.00] 
+
+meusPedido = []
+meusPreco = []
+
+opcao = -1
+ 
+while opcao != 0:
+    print ("1-cafe-> 6.00 2-capuccino->12.00 3-agua->3.00 4-pao frances->15.00 0-sair")
+    opcao = input ("escolha uma opçao:")
---
+++ ss.py
@@ -0,0 +1,8 @@
+usuario = input("usuario: ")
+print (usuario)
+senha = input ("digite a senha: ")
+senhaCorreta = ("passarinho")
+if senha != ("passarinho"):
+    print ("senha incorreta")
+else: 
+    print ("senha correta")
